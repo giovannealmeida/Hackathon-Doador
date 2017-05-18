914790b3fa61080e945c8b0f86f460bb2b36ff16
Alexandre Pedrecal Campos <pedrecal@live.com>
2017-05-18 20:27:36 -0300

    Commit

--- app/src/main/java/br/com/doadorlegal/MainActivity.java
+++ app/src/main/java/br/com/doadorlegal/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import br.com.doadorlegal.fragment.ChartFragment;
 import br.com.doadorlegal.fragment.MainFragment;
