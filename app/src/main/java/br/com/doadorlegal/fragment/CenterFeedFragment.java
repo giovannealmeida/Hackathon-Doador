a925f2ff4e52fda41fd325c71bbab9420731ae2e
Giovanne Almeida <giovanne.almeida91@gmail.com>
2017-05-18 02:58:54 -0300

    Povoando lista

--- app/src/main/java/br/com/doadorlegal/fragment/CenterFeedFragment.java
+++ app/src/main/java/br/com/doadorlegal/fragment/CenterFeedFragment.java
@@ -43,13 +43,20 @@ public class CenterFeedFragment extends Fragment {
 
     private List<Center> getList() {
         List<Center> list = new ArrayList<>();
-        list.add(new Center("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela","08:00 - 16:00","Seg. a Sex."));
-        list.add(new Center("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela","08:00 - 16:00","Seg. a Sex."));
-        list.add(new Center("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela","08:00 - 16:00","Seg. a Sex."));
-        list.add(new Center("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela","08:00 - 16:00","Seg. a Sex."));
-        list.add(new Center("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela","08:00 - 16:00","Seg. a Sex."));
-        list.add(new Center("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela","08:00 - 16:00","Seg. a Sex."));
-        list.add(new Center("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemoacre","Deus seja louvado","A+,AB e O-","Av. Getúlio Vargas, 2787 - Vila Ivonete - Rio Branco, Acre - Cep:69914-500","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemoap","Deus seja louvado","AB e O-","Av Fab, 0 - Central - Macapa, Amapá - Cep:68900-073","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Fhemoam","Deus seja louvado","A+,AB e O-","Av Constantino Nery, 4397 - Chapada - Manaus, Amazonas - Cep:69050-000","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Fundação Hemopa","Deus seja louvado","A+,AB e O-","Travessa Padre Eutiquio, 2109 - Batista Campos - Belem, Pará - Cep:66033-000","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Fhemeron","Deus seja louvado","A+,AB e O-","R Benedito De S Brito, 0 Fhemeron - Industrial - Porto Velho, Rondônia - Cep:76821-080","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Uct De Ji Paraná","Deus seja louvado","A+,AB e O-","R Vilagran Cabrita, 1440 - Centro - Ji-parana, Rondônia - Cep:76900-044","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemocentro De Roraima","Deus seja louvado","A+,AB e O-","Av Brg Eduardo Gomes, 3418 - Aeroporto - Boa Vista, Roraima - Cep:69304-015","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Unidade De Coleta De Transfusão Augustinópolis","Deus seja louvado","A+,AB e O-","Rua Amicuns, 200 - Setor Central - Augustinopolis, Tocantins - Cep:77960-000","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemocentro Coordenador De Palmas","Deus seja louvado","A+,AB e O-","301 Norte, Av. Ns 01, Conj. 02 Lote 01, 0 - Plano Diretor Norte - Palmas, Tocantins - Cep:77015-200","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemocentro Regional De Araguaína","Deus seja louvado","A+,AB e O-","R Treze De Maio, 1336 - Setor Central - Araguaina, Tocantins - Cep:77803-130","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Núcleo De Hemoterapia De Gurupi","Deus seja louvado","A+,AB e O-","R Quatorze De Novembro, 0 Qd: 117 Lote:18 - Setor Central - Gurupi, Tocantins - Cep:77405-070","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemocentro De Alagoas – Hemoal","Deus seja louvado","A+,AB e O-","Av. Jorge De Lima, 58 - Trapiche - Maceio, Alagoas - Cep:57010-300","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemoba","Deus seja louvado","A+,AB e O-","Ld Do Hospital Geral, 0 2º Andar - Brotas - Salvador, Bahia - Cep:40286-240","08:00 - 16:00","Seg. a Sex."));
+        list.add(new Center("Hemoce","Deus seja louvado","A+,AB e O-","R Raimundo Facanha, 3390 - Rodolfo Teofilo - Fortaleza, Ceará - Cep:60431-000","08:00 - 16:00","Seg. a Sex."));
         return list;
     }
 }
--- app/src/main/java/br/com/doadorlegal/fragment/PersonFeedFragment.java
+++ app/src/main/java/br/com/doadorlegal/fragment/PersonFeedFragment.java
@@ -44,13 +44,46 @@ public class PersonFeedFragment extends Fragment {
 
     public List<Person> getList() {
         List<Person> list = new ArrayList<>();
-        list.add(new Person("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela"));
-        list.add(new Person("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela"));
-        list.add(new Person("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela"));
-        list.add(new Person("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela"));
-        list.add(new Person("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela"));
-        list.add(new Person("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela"));
-        list.add(new Person("Hemocentro Universal","Deus seja louvado","A+,AB e O-","Av. Soledade, 1145. Ilha Bela"));
+        list.add(new Person("Homerus Lunnon","Problema renal","O-","Hemoacre"));
+        list.add(new Person("Charlotte O'Collopy","Acidente","AB","Fhemeron"));
+        list.add(new Person("Josey Skea","Problema renal","A+","Hemoba"));
+        list.add(new Person("Annis Campion","Acidente","AB-","Fhemeron"));
+        list.add(new Person("Monroe Colson","Acidente","AB-","Hemoacre"));
+        list.add(new Person("Leilah Grishkov","Tratamento","A-","Hemoba"));
+        list.add(new Person("Carlyn Selbie","Problema renal","B+","Hemoba"));
+        list.add(new Person("Neill Monsey","Tratamento","AB+","Hemoce"));
+        list.add(new Person("Isis Leaver","Problema renal","O+","Hemoce"));
+        list.add(new Person("Gates Bernadzki","Tratamento","B-","Hemoba"));
+        list.add(new Person("Samuele Blackstock","Tratamento","AB+","Hemoacre"));
+        list.add(new Person("Kimbra Olley","Tratamento","AB+","Hemoce"));
+        list.add(new Person("Laina Cecil","Problema renal","B+","Hemoba"));
+        list.add(new Person("Welsh Gasnoll","Acidente","B-","Hemoce"));
+        list.add(new Person("Roseann Dowty","Acidente","A+","Hemoacre"));
+        list.add(new Person("Nanice Ritter","Tratamento","AB-","Hemoce"));
+        list.add(new Person("Katy Longrigg","Problema renal","AB","Hemoba"));
+        list.add(new Person("Meredith L'oiseau","Acidente","O-","Hemoce"));
+        list.add(new Person("Brinna Whiting","Tratamento","B+","Hemoce"));
+        list.add(new Person("Maxy Cutten","Tratamento","O+","Hemoba"));
+        list.add(new Person("Francoise Jentzsch","Tratamento","A+","Hemoce"));
+        list.add(new Person("Tiebout Baurerich","Acidente","A+","Fhemeron"));
+        list.add(new Person("Emlyn Flippelli","Acidente","AB-","Fhemeron"));
+        list.add(new Person("Lu Emmens","Tratamento","B-","Hemoba"));
+        list.add(new Person("Gabe Jex","Tratamento","A+","Hemoce"));
+        list.add(new Person("Arnie Abbott","Tratamento","B+","Hemoacre"));
+        list.add(new Person("Deanna Johanning","Problema renal","B+","Hemoce"));
+        list.add(new Person("Fleur Bree","Acidente","AB+","Hemoce"));
+        list.add(new Person("Harris Schwandner","Acidente","AB+","Hemoba"));
+        list.add(new Person("Michele Yakovlev","Tratamento","AB-","Hemoacre"));
+        list.add(new Person("Flo Tomeo","Tratamento","B-","Hemoce"));
+        list.add(new Person("Reggy Crookes","Problema renal","A+","Hemoce"));
+        list.add(new Person("Ber Tedahl","Problema renal","B+","Fhemeron"));
+        list.add(new Person("Codie Lanon","Problema renal","O-","Hemoacre"));
+        list.add(new Person("Allyson Vine","Tratamento","O+","Hemoba"));
+        list.add(new Person("Suzie Domleo","Tratamento","AB+","Hemoce"));
+        list.add(new Person("Priscilla Ruprechter","Tratamento","AB-","Hemoacre"));
+        list.add(new Person("Robbie Wein","Tratamento","A+","Fhemeron"));
+        list.add(new Person("Nata Quirke","Tratamento","B-","Fhemeron"));
+        list.add(new Person("Kriste Dumbrall","Tratamento","B-","Hemoacre"));
         return list;
     }
 }
